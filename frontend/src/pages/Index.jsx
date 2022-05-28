4d85bbc58870fb99ffb56029a914baa37941128c
Alex Tello <alexindris22@gmail.com>
2022-05-28 14:11:22 +0200

    index

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import banner from '../assets/img/BigPugLoans.jpg'
+import Button from "react-bootstrap/Button";
+import { Link } from 'react-router-dom';
 
 export default function Index() {
   return (
-    <div>HelloWorld</div>
+    <div className='container'>
+      <img src={banner} alt="banner" />
+      <h2>Get your big fat pug loan here</h2>
+      <Link to='/application'>
+        <Button class="btn btn-primary btn-lg">Yes, I want to apply - &gt;</Button>
+      </Link>
+    </div>
   )
 }
 
