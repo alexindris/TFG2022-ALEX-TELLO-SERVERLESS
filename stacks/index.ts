2c8fcd20702142fb15010738dbab28ca433dfe75
Alex Tello <alexindris22@gmail.com>
2022-06-01 16:46:59 +0200

    reduce memory no more is needed

--- stacks/index.ts
+++ stacks/index.ts
@@ -5,6 +5,7 @@ export default function (app: App) {
   app.setDefaultFunctionProps({
     runtime: "nodejs16.x",
     srcPath: "backend",
+    memorySize: 128,
     bundle: {
       format: "esm",
     },
